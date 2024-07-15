41345d42927a503aa53c424deca4720a7c4ac474
codyh587 <108317527+codyh587@users.noreply.github.com>
2024-07-15 11:50:53 -0700

    Implemented route guard on /home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
+
+import { authGuard } from './auth.guard';
 //--- Components ---
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
-    { path: 'home', component: HomeComponent},
-    { path: 'login', component: LoginComponent, pathMatch: 'full' },
-    { path: 'signup', component: SignupComponent}
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] }, // route guard: must be logged in
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
 ];
