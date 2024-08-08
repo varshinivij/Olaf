f1a340fb753ddcdf96ed0374e0c9a61a21347afd
Dylan Riffle <djriffle1@gmail.com>
2024-08-08 15:59:40 -0400

    began adding custom workspace UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { WorkspaceComponent } from './pages/workspace/workspace.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
@@ -13,4 +14,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'workspace', component: WorkspaceComponent},
+
 ];
---
+++ frontend/src/app/pages/workspace/workspace.component.ts
@@ -0,0 +1,177 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
+import { ChatService } from '../../services/chat.service';
+import { SandboxService } from '../../services/sandbox.service';
+import { UploadService } from '../../services/upload.service';
+import { UserService } from '../../services/user.service';
+
+import { ChatMessage } from '../../models/chat-message';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './workspace.component.html',
+  styleUrls: ['./workspace.component.scss'],
+})
+export class WorkspaceComponent implements OnInit, OnDestroy {
+  loading: boolean = false;
+  isConnected: boolean = false;
+  selectedUploadFiles: File[] = [];
+  uploadSubscription: Subscription | undefined;
+  
+  selectedTab: string = 'planner'; // Default tab
+
+  messages: ChatMessage[] = [
+    {
+      type: 'text',
+      role: 'assistant',
+      content: 'Hello, how can I help you today?',
+    },
+  ];
+
+  newMessage: string = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private chatService: ChatService,
+    private sandboxService: SandboxService,
+    public uploadService: UploadService,
+    public userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.uploadSubscription = this.uploadService.getUploadProgress().subscribe(
+      (uploads) => {
+        console.log(uploads);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.uploadSubscription?.unsubscribe();
+  }
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
+
+  connectToSandBox() {
+    this.loading = true;
+    this.sandboxService.createSandbox().subscribe(
+      (response: any) => {
+        this.sandboxService.setSandboxId(response.sandboxId);
+        this.isConnected = true;
+        this.loading = false;
+        console.log(this.sandboxService.getSandboxId());
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      const userMessage: ChatMessage = {
+        type: 'text',
+        role: 'user',
+        content: this.newMessage,
+      };
+      this.messages.push(userMessage);
+      this.newMessage = '';
+      this.loading = true;
+
+      this.chatService.sendMessage(this.messages).subscribe(
+        (response: ChatMessage[]) => {
+          this.processResponse(response);
+          this.messages = [...this.messages, ...response];
+          this.loading = false;
+        },
+        (error) => {
+          console.error('Error:', error);
+          this.loading = false;
+        }
+      );
+    }
+  }
+
+  continue() {
+    this.loading = true;
+    this.chatService.sendMessage(this.messages).subscribe(
+      (response: ChatMessage[]) => {
+        this.messages = [...this.messages, ...response];
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  executeCode(code: string) {
+    this.sandboxService.executeCode(code).subscribe(
+      (result: any) => {
+        const codeResultMessage: ChatMessage = {
+          type: 'text',
+          role: 'assistant',
+          content: result.output || 'Code executed goes here',
+        };
+        this.messages.push(codeResultMessage);
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  processResponse(response: ChatMessage[]) {
+    response.forEach((message) => {
+      if (message.type === 'code') {
+        this.executeCode(message.content);
+      }
+    });
+  }
+
+  onUploadFilesSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+    this.selectedUploadFiles = Array.from(files);
+    this.uploadService.setFiles(this.selectedUploadFiles);
+    console.log('Files selected: ', files);
+  }
+
+  async uploadFiles() {
+    try {
+      await this.uploadService.uploadFiles();
+    } catch (error) {
+      console.error('Error uploading files: ', error);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.userService.logout();
+    } catch (error) {
+      console.error('Error logging out: ', error);
+    }
+  }
+
+  async deleteAccount() {
+    try {
+      await this.userService.deleteAccount();
+    } catch (error) {
+      console.error('Error deleting account: ', error);
+    }
+  }
+}
