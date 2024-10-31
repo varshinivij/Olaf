88ce0d148a5e07bfb5db41c83db6ca10eeea7ffa
codyh587 <108317527+codyh587@users.noreply.github.com>
2024-10-31 11:43:06 -0700

    Swapped in new font (Inter instead of Roboto), added loading/error state to onboarding

--- frontend/src/app/pages/onboarding/onboarding.component.ts
+++ frontend/src/app/pages/onboarding/onboarding.component.ts
@@ -13,15 +13,27 @@ import { UserService } from '../../services/user.service';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmH1Directive } from '@spartan-ng/ui-typography-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+
+import { provideIcons } from '@ng-icons/core';
+import { lucideLoaderCircle } from '@ng-icons/lucide';
 
 @Component({
     selector: 'app-onboarding',
     standalone: true,
-    imports: [ReactiveFormsModule, HlmButtonDirective, HlmInputDirective, HlmH1Directive],
+    imports: [
+        ReactiveFormsModule,
+        HlmButtonDirective,
+        HlmIconComponent,
+        HlmInputDirective,
+        HlmH1Directive,
+    ],
+    providers: [provideIcons({ lucideLoaderCircle })],
     templateUrl: './onboarding.component.html',
     styleUrl: './onboarding.component.scss',
 })
 export class OnboardingComponent {
+    loading = false;
     onboardForm: FormGroup;
     errorMessage: string | null = null;
     private subscription: Subscription | null = null;
@@ -55,14 +67,20 @@ export class OnboardingComponent {
     }
 
     async submitOnboardForm() {
+        this.loading = true;
         try {
-            await this.userService.updateAccount({
-                name: this.onboardForm.value.name,
-            });
+            if (this.onboardForm.value.name.trim() === '') {
+                this.errorMessage = 'Please provide a valid name.';
+            } else {
+                await this.userService.updateAccount({
+                    name: this.onboardForm.value.name,
+                });
+            }
         } catch (error) {
             this.errorMessage =
                 'Account could not be updated. Log in and try again later.';
             console.error('Error updating account: ', error);
         }
+        this.loading = false;
     }
 }
