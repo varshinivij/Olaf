a9852cff9dbf278d0c1d1ef6c23e30b7414eabf5
codyh587 <108317527+codyh587@users.noreply.github.com>
2024-11-07 17:19:00 -0800

    Redesigned sidebar, coloring changes, organization name is now Twocube

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -14,18 +14,14 @@ import Split from 'split.js';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements AfterViewInit {
-  selectedPage: PageName = 'projects';
+  page: PageName = 'projects';
 
   ngAfterViewInit() {
     Split(['#sidebar', '#main-content'], {
-      sizes: [10, 90], // Initial sizes of the columns in percentage
-      minSize: [200, 700], // Minimum size of each column in pixels
+      sizes: [17, 83], // Initial sizes of the columns in percentage
+      minSize: [150, 700], // Minimum size of each column in pixels
       gutterSize: 12, // Size of the gutter (the draggable area between columns)
       snapOffset: 0,
     });
-  }
-
-  selectPage(page: PageName) {
-    this.selectedPage = page;
   }
 }
